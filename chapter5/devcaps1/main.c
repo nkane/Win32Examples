255bbe459f1a3693d408b44c6ace44041368264b
Nick Kane <nkanedev@gmail.com>
2018-03-19 07:13:25 -0400

    adding device capabilities example program

--- chapter5/devcaps1/main.c
+++ chapter5/devcaps1/main.c
@@ -5,7 +5,7 @@
 
 #include <windows.h>
 
-#define numlines((int) (sizeof devcaps / sizeof devcaps[0]))
+#define numlines ((int) (sizeof devcaps / sizeof devcaps[0]))
 
 struct 
 {
@@ -37,4 +37,101 @@ devcaps [] =
     COLORRES,       TEXT("COLORRES"),     TEXT("Actual color resolution:"),
 };
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK
+WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI
+WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
+{
+    static TCHAR szAppName[] = TEXT("DevCaps1");
+    HWND         hwnd;
+    MSG          msg;
+    WNDCLASS     wndclass;
+
+    wndclass.style              = (CS_HREDRAW | CS_VREDRAW);
+    wndclass.lpfnWndProc        = WndProc;
+    wndclass.cbClsExtra         = 0;
+    wndclass.cbWndExtra         = 0;
+    wndclass.hInstance          = hInstance;
+    wndclass.hIcon              = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor            = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground      = (HBRUSH) GetStockObject(WHITE_BRUSH);
+    wndclass.lpszMenuName       = NULL;
+    wndclass.lpszClassName      = szAppName;
+
+    if (!RegisterClass(&wndclass))
+    {
+        MessageBox(NULL, TEXT("Cannot register Window Class!"), szAppName, MB_ICONERROR);
+        return 0;
+    }
+
+    hwnd = CreateWindow(szAppName,
+                        TEXT("Device Capabilities"),
+                        WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT, CW_USEDEFAULT,
+                        CW_USEDEFAULT, CW_USEDEFAULT, 
+                        NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hwnd, iCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return msg.wParam;
+}
+
+LRESULT CALLBACK
+WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    static int  cxChar, cxCaps, cyChar;
+    TCHAR       szBuffer[10];
+    HDC         hdc;
+    int         i;
+    PAINTSTRUCT ps;
+    TEXTMETRIC  tm;
+
+    switch (message)
+    {
+        case WM_CREATE:
+        {
+            hdc = GetDC(hwnd);
+
+            GetTextMetrics(hdc, &tm);
+            cxChar = tm.tmAveCharWidth;
+            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+            cyChar = tm.tmHeight + tm.tmExternalLeading;
+
+            ReleaseDC(hwnd, hdc);
+            return 0;
+        } break;
+
+        case WM_PAINT:
+        {
+            hdc = BeginPaint(hwnd, &ps);
+
+            for (i = 0; i < numlines; i++)
+            {
+                TextOut(hdc, 0, cyChar * i, devcaps[i].szLabel, lstrlen(devcaps[i].szLabel));
+                TextOut(hdc, 14 * cxCaps, cyChar * i, devcaps[i].szDesc, lstrlen(devcaps[i].szDesc));
+                SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+                TextOut(hdc, 14 * cxCaps + 35 * cxChar, cyChar * i, szBuffer, wsprintf(szBuffer, TEXT("%5d"), GetDeviceCaps(hdc, devcaps[i].iIndex)));
+                SetTextAlign(hdc, TA_LEFT | TA_TOP);
+            }
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        } break;
+
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        } break;
+    }
+
+    return DefWindowProc(hwnd, message, wParam, lParam);
+}
